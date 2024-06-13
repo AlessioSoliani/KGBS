6ec726134e0b167110a631bf8466b233cdb4755b
Alessio Soliani <alessiosoliani2gmail.com>
2024-06-13 12:58:53 +0200

    littigando con la responsività

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,37 +1,52 @@
 
-<section class=" w-100  mt-4 container fixed-top">
-  <div class="row justify-content-center">
-    <div class="col-12 col-md-4 d-flex justify-content-center ">
-      <a class="w-100 d-flex justify-content-center navbar-brand" href="#">
-        <img src="media/logNav.png" alt="Logo" width="300" height="60" class="logoStyle  d-inline-block align-text-top">        
-     </a>
-    </div>
+<section class="w-100 mt-4 container fixed-top">
+  <div class="rowLogo row justify-content-center">
+      <div class="col-12 col-md-4 d-flex justify-content-center">
+          <a class="w-100 d-flex justify-content-center navbar-brand" href="#">
+              <img src="media/logNav.png" alt="Logo" width="300" height="60" class="logoStyle d-inline-block align-text-top">
+          </a>
+      </div>
   </div>
   <div class="row mt-4">
-    <div class=" w-100 col-12 col-md-4">
-       <ul class=" list-unstyled w-100 d-flex justify-content-around">
-         <li>           
-           <a class="coloreSecondario text-decoration-none fs-6  " aria-current="page" href="{{route('home')}}">Home</a>                  
-         </li>
-         <li>
-           <a class="coloreSecondario text-decoration-none fs-6" href="{{route('chiSiamo')}}">Chi Siamo</a>                  
-         </li>
-         <li>
-           <a class="coloreSecondario text-decoration-none fs-6" href="/spiritualita">Spiritualità</a>                  
-         </li>
-         <li>
-           <a class="coloreSecondario text-decoration-none fs-6" href="/struttura">Struttura</a>                  
-         </li>
-         <li>
-           <a class="coloreSecondario text-decoration-none fs-6 " aria-current="true" href="/formazione">Formazione</a>                  
-         </li>
-         <li>
-           <a class="coloreSecondario text-decoration-none fs-6 " aria-current="true" href="/storia">Storia</a>
-         </li>        
-       </ul> 
-    </div>
+      <div class="w-100 col-12 col-md-4">
+          <ul class="list-unstyled w-100 d-flex justify-content-around">
+              <li>
+                  <a class="NavancorStyle fs-6" aria-current="page" href="{{route('home')}}">Home</a>
+              </li>
+              <li>
+                  <a class="NavancorStyle fs-6" href="{{route('chiSiamo')}}">Chi Siamo</a>
+              </li>
+              <li>
+                  <a class="NavancorStyle fs-6" href="/spiritualita">Spiritualità</a>
+              </li>
+              <li>
+                  <a class="NavancorStyle fs-6" href="/struttura">Struttura</a>
+              </li>
+              <li>
+                  <a class="NavancorStyle fs-6" aria-current="true" href="/formazione">Formazione</a>
+              </li>
+              <li>
+                  <a class="NavancorStyle fs-6" aria-current="true" href="/storia">Storia</a>
+              </li>
+          </ul>
+      </div>
   </div>
 </section>
+
+<!-- Burger Menu HTML -->
+<nav class="burger-menu">
+  <button class="burger-button" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="burger-icon">burger menù</span>
+  </button>
+  <ul class="burger-links">
+      <li><a class="NavancorStyle fs-6" aria-current="page" href="{{route('home')}}">Home</a></li>
+      <li><a class="NavancorStyle fs-6" href="{{route('chiSiamo')}}">Chi Siamo</a></li>
+      <li><a class="NavancorStyle fs-6" href="/spiritualita">Spiritualità</a></li>
+      <li><a class="NavancorStyle fs-6" href="/struttura">Struttura</a></li>
+      <li><a class="NavancorStyle fs-6" aria-current="true" href="/formazione">Formazione</a></li>
+      <li><a class="NavancorStyle fs-6" aria-current="true" href="/storia">Storia</a></li>
+  </ul>
+</nav>
 
 
 
